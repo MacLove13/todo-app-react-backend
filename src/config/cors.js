6f6cc94e4d190cc4d3ffebb6f86716d963f5a362
MacLove13 <fbarbosa@bionexo.com>
2019-11-28 14:44:45 -0300

    Upload files

---
+++ src/config/cors.js
@@ -0,0 +1,7 @@
+module.exports = function(req, res, next) {
+  res.header('Acess-Control-Allow-Origin', '*')
+  res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATH, DELETE')
+  res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept')
+
+  next()
+}
